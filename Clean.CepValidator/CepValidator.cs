e46e3b48c6d09b533963ce6b70b46cb893f06594
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-03 01:19:05 -0300

    feat: ensure CepValidator validates properly

---
+++ Clean.CepValidator/CepValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clean.CepValidator
+{
+    public static class CepValidator
+    {
+        public static bool Validate(string cep)
+        {
+            string cepRegex = "^[0-9]{5}-[0-9]{3}$";
+            return Regex.IsMatch(cep, cepRegex);
+        }
+    }
+}
