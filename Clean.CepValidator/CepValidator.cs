a6d3f1de9f06c9f39aafcc0eb4a4c98302346ff1
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-03 01:22:54 -0300

    docs: add docstring to CepValidator

--- Clean.CepValidator/CepValidator.cs
+++ Clean.CepValidator/CepValidator.cs
@@ -4,6 +4,11 @@ namespace Clean.CepValidator
 {
     public static class CepValidator
     {
+        /// <summary>
+        /// Validates an cep.
+        /// </summary>
+        /// <param name="cep"></param>
+        /// <returns></returns>
         public static bool Validate(string cep)
         {
             string cepRegex = "^[0-9]{5}-[0-9]{3}$";
